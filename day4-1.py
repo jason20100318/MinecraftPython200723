98e82fdc85f22c97ab77c3ec69eaec9d99f43fe1
jason20100318 <68579356+jason20100318@users.noreply.github.com>
2020-07-23 16:58:06 +0800

    Add files via upload

---
+++ day4-1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 23 09:07:10 2020
+
+@author: user
+"""
+
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+x,y,z=mc.player.getTilePos()
+
+number=1
+
+for i in range(8):
+    for j in range(number):
+        
+        mc.spawnEntity(x,y,z,60)
+
+    number=number*2
+    mc.postToChat("你生成了"+
+                  str(number)+"隻蠹魚")
+
+
+
+
+
---
+++ day4-2.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 23 10:39:11 2020
+
+@author: user
+"""
+
+
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+import random
+x,y,z=mc.player.getTilePos()
+for i in range(20):
+    r=random.randrange(1,5)
+    
+    if r==1:
+        mc.setBlocks(x,y,z,x+4,y,z,41)
+        x=x+4
+    elif r==2:
+        mc.setBlocks(x,y,z,x-4,y,z,46)
+        x=x-4
+    elif r==3:
+        mc.setBlocks(x,y,z,x,y,z+4,137)
+        z=z+4
+    elif r==4:
+        mc.setBlocks(x,y,z,x,y,z-4,56)
+        z=z-4
+    elif r==3:
+        mc.setBlocks(x,y,z,x,y,z+4,137)
+        y=y+4
+    elif r==4:
+        mc.setBlocks(x,y,z,x,y,z-4,56)
+        y=-4
+
